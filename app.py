a38aad7f4aec93ac691b921f22cbd27a091b8859
Erik Larsson <erik.larsson@molflow.com>
2022-12-28 17:24:31 +0100

    Update bucket versions

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ Level1AStack(
     app,
     "Level1AStack",
     rac_bucket_name="ops-payload-level0-v0.1",
-    platform_bucket_name="ops-platform-level1a-v0.2",
-    output_bucket_name="ops-payload-level1a-v0.2",
+    platform_bucket_name="ops-platform-level1a-v0.3",
+    output_bucket_name="ops-payload-level1a-v0.3",
     code_version=f"{repo.tags[-1]} ({repo.head.commit})",
 )
 
