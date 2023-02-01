4c7e165e758fb1bcfa8637a27d00ad0452a28d1b
Andreas Skyman <andreas.skyman@molflow.com>
2023-02-01 09:04:52 +0100

    Bump in- and output versions

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ repo = git.Repo(".")
 Level1AStack(
     app,
     "Level1AStack",
-    rac_bucket_name="ops-payload-level0-v0.1",
+    rac_bucket_name="ops-payload-level0-v0.2",
     platform_bucket_name="ops-platform-level1a-v0.3",
-    output_bucket_name="ops-payload-level1a-v0.3",
+    output_bucket_name="ops-payload-level1a-v0.4",
     code_version=f"{repo.tags[-1]} ({repo.head.commit})",
 )
 
