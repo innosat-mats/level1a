97f46af2e0f72650c1f5ea822b060b98c8764715
Andreas Skyman <andreas.skyman@molflow.com>
2023-02-15 16:14:08 +0100

    Bump outbucket version

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ Level1AStack(
     "Level1AStack",
     rac_bucket_name="ops-payload-level0-v0.2",
     platform_bucket_name="ops-platform-level1a-v0.3",
-    output_bucket_name="ops-payload-level1a-v0.4",
+    output_bucket_name="ops-payload-level1a-v0.5",
     code_version=f"{repo.tags[-1]} ({repo.head.commit})",
 )
 
