07f7d57b79da9895a506e2775366a5042465d2c9
Andreas Skyman <andreas.skyman@molflow.com>
2023-02-21 12:53:22 +0100

    Protect against missing tag

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ from stacks.level1a_stack import Level1AStack
 app = App()
 repo = git.Repo(".")
 
+try:
+    tag = repo.tags[-1]
+except IndexError:
+    tag = None
+
 Level1AStack(
     app,
     "Level1AStack",
     rac_bucket_name="ops-payload-level0-v0.2",
     platform_bucket_name="ops-platform-level1a-v0.3",
     output_bucket_name="ops-payload-level1a-v0.5",
-    code_version=f"{repo.tags[-1]} ({repo.head.commit})",
+    code_version=f"{tag} ({repo.head.commit})",
 )
 
 app.synth()
