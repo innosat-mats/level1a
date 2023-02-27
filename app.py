04e03282ad876e9b90a776a651128e5052e38389
Andreas Skyman <andreas.skyman@molflow.com>
2023-02-27 15:01:00 +0100

    No upper case letters in bucket names

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ Level1AStack(
     "Level1AStackPM",
     rac_bucket_name="ops-payload-level0-v0.2",
     platform_bucket_name="ops-platform-level1a-v0.3",
-    output_bucket_name="ops-payload-level1a-PM-v0.1",
+    output_bucket_name="ops-payload-level1a-pm-v0.1",
     code_version=f"{tag} ({repo.head.commit})",
     data_prefix="PM",
     read_htr=False,
